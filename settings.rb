e9d090655c3e6bf9d08c4d7cfdc2bc11f154f7ee
辻大地 <tsujidaichi@macoz.local>
2017-11-20 18:07:03 +0900

    bugfix

--- settings.rb
+++ settings.rb
@@ -7,6 +7,6 @@
 @regular_times    = parse_regular_times("8:40, 12:10, 15:00, 18:00")
 @morning_time     = ENV['JOB_KICK_MORNING_TIME']     || "08:30"
 @noon_time        = ENV['JOB_KICK_NOON_TIME']        || "12:00"
-@after_noon_time  = ENV['JOB_KICK_AFTER_NOON_TIME']  || "18:00"
+@after_noon_time  = ENV['JOB_KICK_AFTER_NOON_TIME']  || "18:15"
 @night_time       = ENV['JOB_KICK_NIGHT_TIME']       || "23:29"
 @dailyreport_time = ENV['JOB_KICK_DAILYREPORT_TIME'] || "23:30"
